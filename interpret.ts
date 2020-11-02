2d8bc8c648a986d598c117dc4463681f0539b40f
divy-work <dj.srivastava23@gmail.com>
2020-11-02 17:56:49 +0530

    refract(interpreter): less error prone evaluation

--- interpret.ts
+++ interpret.ts
@@ -5,7 +5,7 @@ const BUFFER_SIZE = 65536; // 32 * 64 KiB
 
 interface Node {
   type: string;
-  data: string;
+  data: number | Node[];
 }
 
 class Interpreter {
@@ -17,25 +17,44 @@ class Interpreter {
 
     this.buffer = new Uint32Array(BUFFER_SIZE);
     this.pointer = 0;
-
   }
 
-  increaseBufferSize() {
+  increaseBufferSize = () => {
     const buffer = new Uint32Array(this.buffer.byteLength + BUFFER_SIZE);
     buffer.set(this.buffer);
     this.buffer = buffer;
   }
   
-  run(node: Node) { 
-    // @ts-ignore
-    return this[node.type](node.data) 
+  public run = (node: Node) => { 
+    if (Array.isArray(node.data)) {
+        if(node.type === "Program") {
+          return this.Program(node.data);
+        } else if(node.type === "Loop") {
+          return this.Loop(node.data);
+        }
+        return;
+    }
+    switch (node.type) {
+      case "Pointer":
+        return this.Pointer(node.data);
+      case "Value":
+        return this.Value(node.data);
+      case "Zero":
+        return this.Zero();
+      case "GetChar":
+        return this.GetChar();
+      case "PutChar":
+        return this.PutChar();
+      default:
+        console.log(node)
+    }
   }
 
-  Program(nodes: Node[]) {
+  Program = (nodes: Node[]) => {
     nodes.forEach(this.run);
   }
 
-  Pointer(diff: number) {
+  Pointer = (diff: number) => {
     this.pointer += diff;
 
     while (this.pointer >= this.buffer.byteLength) {
@@ -45,32 +64,32 @@ class Interpreter {
     if (this.pointer < 0) {
       const n = this.pointer;
       const m = this.buffer.byteLength;
-      this.pointer = ((n % m) + m) % m; // positive modulo
+      this.pointer = ((n % m) + m) % m; // => Positive modulo
     }
   }
 
-  Value(diff: number) {
+  Value = (diff: number) => {
     this.buffer[this.pointer] += diff;
   }
 
-  Zero() {
+  Zero = () => {
     this.buffer[this.pointer] = 0;
   }
 
-  Mul({ pointerDiff, valueDiff }: { pointerDiff: number; valueDiff: number; }) {
+  Mul = ({ pointerDiff, valueDiff }: { pointerDiff: number; valueDiff: number; }) => {
     this.buffer[this.pointer + pointerDiff] +=
       this.buffer[this.pointer] * valueDiff;
   }
 
-  GetChar() {
+  GetChar = () => {
     this.buffer[this.pointer] = this.lib.getChar();
   }
 
-  PutChar() {
+  PutChar = () => {
     this.lib.putChar(this.buffer[this.pointer]);
   }
 
-  Loop(nodes: Node[]) {
+  Loop = (nodes: Node[]) => {
     if (!this.buffer[this.pointer]) return;
 
     let idx = 0;
